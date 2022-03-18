c0f38c3efc790fee47243fb986efd195c61eb12d
Katalin Kovacs <katalin_kovacs@epam.com>
2022-03-18 13:46:20 +0100

    Module-1: Tasks are done

--- module-1/euclidean.js
+++ module-1/euclidean.js
@@ -7,18 +7,28 @@
  * @returns {number} GCD or 0 if any arguments are not proper
  */
 function euclidean(a, b) {
-    let gcd;
-    /*
-     * Your task is to compute the greatest common divisor of
-     * the numbers given in a and b variables, using the
-     * Euclidean algorithm (https://en.wikipedia.org/wiki/Euclidean_algorithm).
-     * If you have the result, assign it to the variable, called gcd.
-     * Also take into consideration the documentation of the function!
-     */
-    // PLACE YOUR CODE BETWEEN THIS...
-
-
-    // ...AND THIS COMMENT LINE!
-    return gcd;
+  let gcd;
+  /*
+   * Your task is to compute the greatest common divisor of
+   * the numbers given in a and b variables, using the
+   * Euclidean algorithm (https://en.wikipedia.org/wiki/Euclidean_algorithm).
+   * If you have the result, assign it to the variable, called gcd.
+   * Also take into consideration the documentation of the function!
+   */
+  // PLACE YOUR CODE BETWEEN THIS...
+  if (a > 0 && b > 0) {
+    while (a !== b) {
+      if (a > b) {
+        a = a - b;
+      } else {
+        b = b - a;
+      }
+    }
+    gcd = a;
+  } else {
+    gcd = 0;
+  }
+  // ...AND THIS COMMENT LINE!
+  return gcd;
 }
 module.exports = euclidean;
--- module-1/fibonacci.js
+++ module-1/fibonacci.js
@@ -6,17 +6,35 @@
  * @returns {number} Fibonacci number or 0 if any arguments are not proper
  */
 function fibonacci(n) {
-    let nThFibonacci;
-    /*
-     * Your task is to calculate the nth value of the
-     * Fibonacci sequence.
-     * https://en.wikipedia.org/wiki/Fibonacci_number
-     * Store the value in the nThFibonacci variable.
-     * Also take into consideration the documentation of the function!
-     */
-    // PLACE YOUR CODE BETWEEN THIS...
+  let nThFibonacci;
+  /*
+   * Your task is to calculate the nth value of the
+   * Fibonacci sequence.
+   * https://en.wikipedia.org/wiki/Fibonacci_number
+   * Store the value in the nThFibonacci variable.
+   * Also take into consideration the documentation of the function!
+   */
+  // PLACE YOUR CODE BETWEEN THIS...
+  if (n >= 0) {
+    if (n < 2) {
+      nThFibonacci = n;
+    } else {
+      //Recursive way
+      //nThFibonacci = fibonacci(n - 2) + fibonacci(n - 1);
 
-    // ...AND THIS COMMENT LINE!
-    return nThFibonacci;
+      let f0 = 0;
+      let f1 = 1;
+      for (let i = 2; i <= n; i++) {
+        const sum = f0 + f1;
+        f0 = f1;
+        f1 = sum;
+      }
+      nThFibonacci = f1;
+    }
+  } else {
+    nThFibonacci = 0;
+  }
+  // ...AND THIS COMMENT LINE!
+  return nThFibonacci;
 }
 module.exports = fibonacci;
