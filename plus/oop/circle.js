ad9f2a63613bc402e323f093e46470c205e43c53
Katalin Kovacs <katalin_kovacs@epam.com>
2022-03-22 20:56:08 +0100

    Circle done

--- plus/oop/circle.js
+++ plus/oop/circle.js
@@ -6,3 +6,20 @@
  * @function area return the area of the circle
  * @function perimeter return the perimeter of the circle
  */
+
+module.exports = class Circle {
+  constructor(r) {
+    if (r < 0) {
+      throw new Error("Invalid radius.");
+    }
+    this.r = r;
+  }
+
+  getArea() {
+    return this.r * this.r * Math.PI;
+  }
+
+  getPerimeter() {
+    return 2 * this.r * Math.PI;
+  }
+};
